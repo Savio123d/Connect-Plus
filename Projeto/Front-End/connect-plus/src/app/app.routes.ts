ab47a3f0e134f3512a268f9f29c834b489a39ec9
Savio Henrique <savio13foxy@gmail.com>
2026-03-15 21:39:34 -0300

    Update app.routes.ts
    routas defenidas

--- Projeto/Front-End/connect-plus/src/app/app.routes.ts
+++ Projeto/Front-End/connect-plus/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard-usuario',
+  },
+  {
+    path: 'dashboard-usuario',
+    loadComponent: () =>
+      import('../dashboardUsuario/dashboard-usuario').then(
+        (module) => module.DashboardUsuario,
+      ),
+  },
+  {
+    path: 'dashboard-gestor',
+    loadComponent: () =>
+      import('../dashboardGestor/dashboard-gestor').then(
+        (module) => module.DashboardGestor,
+      ),
+  },
+];
