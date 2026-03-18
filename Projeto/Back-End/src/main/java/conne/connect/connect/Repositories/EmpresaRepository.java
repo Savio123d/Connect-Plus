d75e7d5c222f210f5954082d9f9577829cee14e8
Savio123d <savio13foxy@gmail.com>
2026-03-17 21:48:26 -0300

    feat(be): CON-30 criar CRUD de empresa

---
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Repositories/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package conne.connect.connect.Repositories;
+
+import conne.connect.connect.Models.EmpresaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<EmpresaModel, Long> {
+}
