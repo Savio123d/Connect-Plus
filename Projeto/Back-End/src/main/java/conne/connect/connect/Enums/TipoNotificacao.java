a7560887c9e8be6a2e630cb7916d3d381cd1fc71
Savio123d <savio13foxy@gmail.com>
2026-03-18 00:21:15 -0300

    feat(be): CON-30 criar CRUD de notificacao

---
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Enums/TipoNotificacao.java
@@ -0,0 +1,11 @@
+package conne.connect.connect.Enums;
+
+public enum TipoNotificacao {
+    novo_projeto,
+    nova_tarefa,
+    tarefa_concluida,
+    lembrete_prazo,
+    mensagem,
+    feedback,
+    sistema
+}
---
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Repositories/NotificacaoRepository.java
@@ -0,0 +1,7 @@
+package conne.connect.connect.Repositories;
+
+import conne.connect.connect.Models.NotificacaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificacaoRepository extends JpaRepository<NotificacaoModel, Long> {
+}
