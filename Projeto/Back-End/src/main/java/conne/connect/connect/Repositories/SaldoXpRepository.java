0717435b18a9c071dd9728dad64e2b57a00fa3ef
Savio123d <savio13foxy@gmail.com>
2026-03-18 00:09:37 -0300

    feat(be): CON-30 criar CRUD de saldo xp

---
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Repositories/SaldoXpRepository.java
@@ -0,0 +1,7 @@
+package conne.connect.connect.Repositories;
+
+import conne.connect.connect.Models.SaldoXpModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaldoXpRepository extends JpaRepository<SaldoXpModel, Long> {
+}
