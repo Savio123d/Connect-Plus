fe23467497e5df3d4740fa0369cff66ecbe4dbef
Savio123d <savio13foxy@gmail.com>
2026-03-24 14:48:25 -0300

    Update cadastro-empresa.spec.ts
    refactor(cadastro-empresa):mundança de nome

--- Projeto/Front-End/connect-plus/src/cadastro-empresa/cadastro-empresa.spec.ts
+++ Projeto/Front-End/connect-plus/src/cadastro-empresa/cadastro-empresa.spec.ts
@@ -1,6 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CadastroEmpresa } from './cadastro-empresa-page';
+import { CadastroEmpresa } from './cadastro-empresa';
 
 describe('CadastroEmpresa', () => {
   let component: CadastroEmpresa;
@@ -9,6 +10,7 @@ describe('CadastroEmpresa', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CadastroEmpresa],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CadastroEmpresa);
