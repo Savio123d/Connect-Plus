0dbdbe6d9b5c66696b5c255ae5203aed69cef897
Savio Henrique <savio13foxy@gmail.com>
2026-03-27 23:17:38 -0300

    Update app.config.ts
    chore: tirando coisas que não necessita no momento

--- Projeto/Front-End/connect-plus/src/app/app.config.ts
+++ Projeto/Front-End/connect-plus/src/app/app.config.ts
@@ -1,13 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideHttpClient(),
     provideRouter(routes),
   ],
 };
