0e642f45f00905b046d8fd5c730ddb7e38492e5e
Raphael <raphaelzzz72@gmail.com>
2026-04-01 03:39:12 -0300

    CON-55t: cria tela de visualização de usuário

--- Projeto/Front-End/connect-plus/src/app/app.routes.ts
+++ Projeto/Front-End/connect-plus/src/app/app.routes.ts
@@ -1,8 +1,4 @@
-import { Menu } from './../pages/dashboard/dashboard';
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { CadastroEmpresa } from './pages/cadastro-empresa/cadastro-empresa';
 
 export const routes: Routes = [
   {
@@ -13,22 +9,29 @@ export const routes: Routes = [
   {
     path: 'cadastro-empresa',
     loadComponent: () =>
-      import('../pages/cadastro-empresa/cadastro-empresa').then(
+      import('./pages/cadastro-empresa/cadastro-empresa').then(
         (module) => module.CadastroEmpresa,
       ),
   },
   {
     path: 'dashboard',
     loadComponent: () =>
-      import('../pages/dashboard/dashboard').then(
+      import('./pages/dashboard/dashboard').then(
         (module) => module.Menu,
       ),
   },
   {
     path: 'login',
     loadComponent: () =>
-      import('../pages/login/login').then(
+      import('./pages/login/login').then(
         (module) => module.Login,
+      ),
+  },
+  {
+        path: 'usuarios',
+    loadComponent: () =>
+      import('./pages/usuarios/usuarios').then(
+        (module) => module.Usuarios,
       ),
   }
 ];
--- Projeto/Front-End/connect-plus/src/app/pages/dashboard/dashboard.spec.ts
+++ Projeto/Front-End/connect-plus/src/app/pages/dashboard/dashboard.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Dashboard } from './dashboard';
+import { Menu } from './dashboard';
 
-describe('Dashboard', () => {
-  let component: Dashboard;
-  let fixture: ComponentFixture<Dashboard>;
+describe('Menu', () => {
+  let component: Menu;
+  let fixture: ComponentFixture<Menu>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Dashboard],
+      imports: [Menu]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Dashboard);
+    fixture = TestBed.createComponent(Menu);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
