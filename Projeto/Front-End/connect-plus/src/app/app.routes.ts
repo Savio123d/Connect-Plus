82c3abe3394cd7c648aa3345a4d626184569e878
Savio Henrique <savio13foxy@gmail.com>
2026-04-05 19:33:29 -0300

    Update app.routes.ts
    refactor: mundaça de linha

--- Projeto/Front-End/connect-plus/src/app/app.routes.ts
+++ Projeto/Front-End/connect-plus/src/app/app.routes.ts
@@ -9,29 +9,18 @@ export const routes: Routes = [
   {
     path: 'cadastro-empresa',
     loadComponent: () =>
-      import('./pages/cadastro-empresa/cadastro-empresa').then(
-        (module) => module.CadastroEmpresa,
-      ),
+      import('./pages/cadastro-empresa/cadastro-empresa').then((module) => module.CadastroEmpresa),
   },
   {
     path: 'dashboard',
-    loadComponent: () =>
-      import('./pages/dashboard/dashboard').then(
-        (module) => module.Menu,
-      ),
+    loadComponent: () => import('./pages/dashboard/dashboard').then((module) => module.Menu),
   },
   {
     path: 'login',
-    loadComponent: () =>
-      import('./pages/login/login').then(
-        (module) => module.Login,
-      ),
+    loadComponent: () => import('./pages/login/login').then((module) => module.Login),
   },
   {
-        path: 'usuarios',
-    loadComponent: () =>
-      import('./pages/usuarios/usuarios').then(
-        (module) => module.Usuarios,
-      ),
-  }
+    path: 'usuarios',
+    loadComponent: () => import('./pages/usuarios/usuarios').then((module) => module.Usuarios),
+  },
 ];
