884f19ee4f558ff1c98a37ea670316c4556b6893
Gustavo Antonio <gustavoantonio0512@gmail.com>
2026-04-30 21:37:27 -0300

    feat: adiciona edição e exclusão de tarefas no kanban

---
+++ Projeto/Front-End/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'cadastro-empresa',
+    loadComponent: () =>
+      import('./pages/cadastro-empresa/cadastro-empresa').then(
+        (module) => module.CadastroEmpresa,
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard').then(
+        (module) => module.Menu,
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then(
+        (module) => module.Login,
+      ),
+  },
+  {
+        path: 'usuarios',
+    loadComponent: () =>
+      import('./pages/usuarios/usuarios').then(
+        (module) => module.Usuarios,
+      ),
+  }
+];
