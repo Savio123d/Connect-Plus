46e05cbc9b94f9c383770a4cb77e0945dfc69f7e
Savio Henrique <savio13foxy@gmail.com>
2026-05-25 12:43:36 -0300

    reafctor: usando lombok no dto

--- Projeto/Back-End/src/main/java/conne/connect/connect/Dto/CadastroEmpresaDTO.java
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Dto/CadastroEmpresaDTO.java
@@ -3,7 +3,13 @@ package conne.connect.connect.Dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class CadastroEmpresaDTO {
 
     @NotBlank
@@ -15,6 +21,7 @@ public class CadastroEmpresaDTO {
     private String cnpj;
 
     private String cidade;
+
     private String uf;
 
     @NotBlank
@@ -27,68 +34,4 @@ public class CadastroEmpresaDTO {
     @NotBlank
     @Size(min = 8)
     private String senhaAdmin;
-
-    public String getRazaoSocial() {
-        return razaoSocial;
-    }
-
-    public void setRazaoSocial(String razaoSocial) {
-        this.razaoSocial = razaoSocial;
-    }
-
-    public String getNomeFantasia() {
-        return nomeFantasia;
-    }
-
-    public void setNomeFantasia(String nomeFantasia) {
-        this.nomeFantasia = nomeFantasia;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public String getNomeAdmin() {
-        return nomeAdmin;
-    }
-
-    public void setNomeAdmin(String nomeAdmin) {
-        this.nomeAdmin = nomeAdmin;
-    }
-
-    public String getEmailAdmin() {
-        return emailAdmin;
-    }
-
-    public void setEmailAdmin(String emailAdmin) {
-        this.emailAdmin = emailAdmin;
-    }
-
-    public String getSenhaAdmin() {
-        return senhaAdmin;
-    }
-
-    public void setSenhaAdmin(String senhaAdmin) {
-        this.senhaAdmin = senhaAdmin;
-    }
 }
