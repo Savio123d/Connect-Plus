bca8222a629adc30c14561419259dcb574198e72
Gustavo Antonio <gustavoantonio0512@gmail.com>
2026-07-01 02:40:20 -0300

    Refactor: Alteração para se encaixar nos feedbacks

--- Projeto/Back-End/src/main/java/conne/connect/connect/Projeto/repository/ProjetoTelaRepository.java
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Projeto/repository/ProjetoTelaRepository.java
@@ -1,6 +1,7 @@
 package conne.connect.connect.Projeto.repository;
 
 import conne.connect.connect.Projeto.model.ProjetoTelaModel;
+import conne.connect.connect.Projeto.enums.ProjetoStatusTela;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,10 @@ public interface ProjetoTelaRepository extends JpaRepository<ProjetoTelaModel, L
     List<ProjetoTelaModel> findAllByOrderByIdProjetoDesc();
 
     List<ProjetoTelaModel> findByEmpresa_IdEmpresaOrderByIdProjetoDesc(Long empresaId);
+
+    List<ProjetoTelaModel> findByEmpresa_IdEmpresaAndStatusAndConcluidoEmIsNotNullOrderByConcluidoEmDesc(
+            Long empresaId,
+            ProjetoStatusTela status
+    );
 }
+
