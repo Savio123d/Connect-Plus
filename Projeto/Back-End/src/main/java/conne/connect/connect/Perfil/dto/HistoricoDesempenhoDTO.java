c8c2d5f656cd604b554ee782eaeecf08c0e644a7
GUILHERME HENRIQUE COSTA <guicosta0601@gmail.com>
2026-07-07 00:07:46 -0300

    Update HistoricoDesempenhoDTO.java

--- Projeto/Back-End/src/main/java/conne/connect/connect/Perfil/dto/HistoricoDesempenhoDTO.java
+++ Projeto/Back-End/src/main/java/conne/connect/connect/Perfil/dto/HistoricoDesempenhoDTO.java
@@ -1,17 +1,8 @@
 package conne.connect.connect.Perfil.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class HistoricoDesempenhoDTO {
-
-    private String mes;
-    private Integer tarefasConcluidas;
-    private Integer xpGanho;
+public record HistoricoDesempenhoDTO(
+        String mes,
+        Long tarefasConcluidas,
+        Long xpGanho
+) {
 }
